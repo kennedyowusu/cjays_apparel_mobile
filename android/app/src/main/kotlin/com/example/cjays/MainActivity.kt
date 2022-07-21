f9e3146ff9604652c0f7acaaa98461d4e2e69c19
Kennedy Owusu <kennedyowusu@gmail.com>
2022-07-21 22:29:07 +0000

    project set up

---
+++ android/app/src/main/kotlin/com/example/cjays/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cjays
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/home/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Demo'),
+      ),
+      body: Center(
+        child: const Text('Hello World'),
+      ),
+    );
+  }
+}
